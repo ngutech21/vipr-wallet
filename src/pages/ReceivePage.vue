65c77ba8014f7d0781240fc8300852a2f02ae4bf
ngutech21 <ngutech21@pm.me>
2025-02-05 17:10:38 +0100

    chore: cleanup receive page

--- src/pages/ReceivePage.vue
+++ src/pages/ReceivePage.vue
@@ -7,42 +7,60 @@
         <q-btn icon="arrow_back" label="Back" flat class="q-mb-lg" :to="'/'" />
 
         <!-- Enter amount textfield -->
-        <div class="q-mb-md">
-          <q-input filled v-model.number="amount" label="Amount" type="number" />
+        <div class="q-mb-md" v-if="!qrData">
+          <q-input
+            filled
+            v-model.number="amount"
+            label="Amount (Sats)"
+            type="number"
+            ref="amountInput"
+          />
         </div>
+        <div class="h-6 heading-text">Pay the lightning invoice to receive your ecash.</div>
 
         <!-- QR Code Card -->
-        <q-card v-if="qrData">
+        <q-card v-if="qrData" class="input-width">
           <q-card-section>
-            <qrcode-vue :value="qrData" :size="512" level="M" bgColor="#ffffff" fgColor="#000000" />
-            <q-card-section>
-              <q-input filled v-model="qrData" label="Lightning Invoice" readonly />
-              <q-btn icon="content_copy" label="Copy" flat @click="copyToClipboard" />
+            <qrcode-vue :value="qrData" :size="480" level="M" bgColor="#ffffff" fgColor="#000000" />
+            <q-card-section class="row">
+              <q-input filled v-model="qrData" label="Lightning Invoice" readonly class="col-10" />
+              <q-btn icon="content_copy" label="Copy" flat @click="copyToClipboard" class="col-2" />
             </q-card-section>
           </q-card-section>
         </q-card>
 
         <!-- Request button -->
-        <q-btn label="Request" color="primary" @click="onRequest" />
+        <q-btn label="Create Invoice" color="primary" @click="onRequest" v-if="!qrData" />
       </q-page>
     </q-page-container>
   </q-layout>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import QrcodeVue from 'qrcode.vue'
 import { useFedimintStore } from 'src/stores/fedimint'
 
 const amount = ref<number>(0)
 const qrData = ref('')
 const store = useFedimintStore()
+const amountInput = ref<HTMLInputElement | null>(null)
+
+onMounted(() => {
+  if (amountInput.value) {
+    amountInput.value.focus()
+  }
+})
 
 async function onRequest() {
   // Handle request logic here
 
+  if (amount.value < 1) {
+    return
+  }
+
   console.log('Requesting amount:', amount.value)
-  const invoice = await store.wallet?.lightning.createInvoice(amount.value, 'minting tokens')
+  const invoice = await store.wallet?.lightning.createInvoice(amount.value * 1_000, 'minting ecash')
 
   if (invoice) {
     qrData.value = invoice.invoice
@@ -54,3 +72,14 @@ async function copyToClipboard() {
   await navigator.clipboard.writeText(qrData.value)
 }
 </script>
+
+<style scoped>
+.input-width {
+  width: 100%;
+  max-width: 512px;
+}
+
+.heading-text {
+  font-size: 1.5rem; /* Adjust the font size as needed */
+}
+</style>
--- src/pages/ScanPage.vue
+++ src/pages/ScanPage.vue
@@ -1,32 +1,35 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-page-container>
-      <q-page class="row items-center justify-evenly">
-        <q-card class="q-px-md q-pt-md q-pb-xl full-width">
-          <q-card-section>
-            <div class="text-h6">Scan QR Code</div>
-          </q-card-section>
+      <q-page class="padding">
+        <q-btn icon="arrow_back" label="Back" flat class="q-mb-lg" :to="'/'" />
+        <div class="row items-center justify-evenly">
+          <q-card class="q-px-md q-pt-md q-pb-xl full-width">
+            <q-card-section>
+              <div class="text-h6">Scan QR Code</div>
+            </q-card-section>
 
-          <q-separator />
+            <q-separator />
 
-          <q-card-section>
-            <qrcode-stream @detect="onDetect" @init="onInit" />
-          </q-card-section>
+            <q-card-section>
+              <qrcode-stream @detect="onDetect" @init="onInit" />
+            </q-card-section>
 
-          <q-card-section>
-            <q-input
-              filled
-              label="Detected QR Code"
-              v-model="detectedContent"
-              readonly
-              type="textarea"
-            />
-          </q-card-section>
+            <q-card-section>
+              <q-input
+                filled
+                label="Detected QR Code"
+                v-model="detectedContent"
+                readonly
+                type="textarea"
+              />
+            </q-card-section>
 
-          <q-card-section>
-            <q-btn label="Close" color="primary" :to="'/'" />
-          </q-card-section>
-        </q-card>
+            <q-card-section>
+              <q-btn label="Close" color="primary" :to="'/'" />
+            </q-card-section>
+          </q-card>
+        </div>
       </q-page>
     </q-page-container>
   </q-layout>
