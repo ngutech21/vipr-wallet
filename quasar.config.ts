daedbe930a707ba3b647d153dd125d6d19e7372e
ngutech21 <ngutech21@pm.me>
2025-02-17 14:51:39 +0100

    fix: include DIalog plugin

--- quasar.config.ts
+++ quasar.config.ts
@@ -137,7 +137,7 @@ export default defineConfig((/* ctx */) => {
       // directives: [],
 
       // Quasar plugins
-      plugins: ['Notify', 'Loading'],
+      plugins: ['Notify', 'Loading', 'Dialog'],
     },
 
     // animations: 'all', // --- includes all animations
