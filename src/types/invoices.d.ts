474dc7df2c44098118af8ab3c07e723acbc52ff4
Steffen <99717310+ngutech21@users.noreply.github.com>
2025-02-18 23:49:22 +0100

    fix: lint error

--- src/types/invoices.d.ts
+++
@@ -1,7 +0,0 @@
-//import type { Bolt11Invoice } from 'src/components/models'
-
-declare module 'invoices' {
-  export function byteDecodeRequest(invoice: string): Bolt11Invoice
-
-  export function byteEncodeRequest(params: Bolt11Invoice): string
-}
