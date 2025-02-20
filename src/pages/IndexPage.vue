e69e3b2b958fee2c18d3543e26957f1704f33709
ngutech21 <ngutech21@pm.me>
2025-02-20 15:16:27 +0100

    feat: show add federation button if no federation is present

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,14 @@
 <template>
   <q-page class="column">
+    <q-dialog
+      v-model="showAddFederationOverlay"
+      position="bottom"
+      transition-show="slide-up"
+      transition-hide="slide-down"
+    >
+      <AddFederationPage @close="showAddFederationOverlay = false" />
+    </q-dialog>
+
     <div class="text-white q-pa-md dark-bg" style="width: 100%">
       <div class="text-h4 text-center">{{ totalBalance.toLocaleString() }} sats</div>
 
@@ -13,8 +22,28 @@
 
     <TransactionsList />
 
+    <div
+      v-if="federationStore.federations.length == 0"
+      class="column items-center justify-center full-height"
+      style="flex: 1"
+    >
+      <div class="text-h6">Ready to start?</div>
+      <div class="text-subtitle7">Add a Federation to get up and running</div>
+      <q-btn
+        label="Join a federation"
+        color="primary"
+        class="q-mt-md"
+        icon="add"
+        @click="showAddFederationOverlay = true"
+      />
+    </div>
+
     <!-- Added fixed bottom buttons using q-page-sticky -->
-    <q-page-sticky position="bottom" :offset="[0, 100]">
+    <q-page-sticky
+      position="bottom"
+      :offset="[0, 100]"
+      v-if="federationStore.federations.length > 0"
+    >
       <div class="q-pa-md">
         <div class="row items-center justify-evenly q-gutter-md">
           <q-btn label="Send" icon="arrow_upward" color="primary" :to="'/send'" />
@@ -27,15 +56,17 @@
 </template>
 
 <script setup lang="ts">
-import { computed, onMounted } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import { useFederationStore } from 'src/stores/federation'
 import { useWalletStore } from 'src/stores/wallet'
 import TransactionsList from 'src/components/TransactionsList.vue'
+import AddFederationPage from './AddFederationPage.vue'
 
 const federationStore = useFederationStore()
 const selectedFederation = computed(() => federationStore.selectedFederation)
 const walletStore = useWalletStore()
 const totalBalance = computed(() => walletStore.balance)
+const showAddFederationOverlay = ref(false)
 
 onMounted(() => {
   console.log('Joining Fedimint...')
