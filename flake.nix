2d2936e5a512ebe1d194e8921f813f79ff75d2a0
ngutech21 <ngutech21@pm.me>
2025-03-08 14:05:28 +0100

    chore: add nix dev shell

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Vipr-Wallet development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_20
+            nodePackages.pnpm
+            nodePackages.typescript
+            nodePackages.quasar-cli
+          ];
+
+          shellHook = ''
+            echo "Vipr-Wallet development environment"
+            echo "Node.js $(node --version)"
+            echo "pnpm $(pnpm --version)"
+            echo "Quasar CLI $(quasar --version)"
+            echo "TypeScript $(tsc --version)"
+            echo ""
+            echo "Run 'pnpm install' to set up dependencies"
+            echo "Then 'pnpm dev' to start the development server"
+          '';
+        };
+      }
+    );
+}
